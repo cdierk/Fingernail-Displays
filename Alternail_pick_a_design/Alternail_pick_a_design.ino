cf79f219ccca9f64ad1a5a631020c02c69c38934
cdierk <cdierk@elon.edu>
2016-09-15 11:48:07 -0700

    Alternail_usage added
    Code for points/usage/counter application of AlterNail

--- Alternail_pick_a_design/Alternail_pick_a_design.ino
+++ Alternail_pick_a_design/Alternail_pick_a_design.ino
@@ -14,6 +14,14 @@
 #define EXTRA_DOT 5     //reset pin 
 #define REFRESH_DELAY 500 
 
+const int numReadings = 10;
+
+int readings[numReadings];      // the readings from the analog input
+int readIndex = 0;              // the index of the current reading
+int total = 0;                  // the running total
+int average = 0;                // the average
+int current_state = 0;          // 0: only 1 dot; 1: all 3 dots
+
 void clearDisplay()
 {
 
@@ -28,31 +36,68 @@ void setup() {
   pinMode(TOP_DOT, OUTPUT);
   pinMode(MIDDLE_DOT, OUTPUT);
   pinMode(BOTTOM_DOT, OUTPUT);
-  clearDisplay();
+  current_state = -1;             //no dots showing
+  clearDisplay();   
+  delay(500); //to avoid initial bonky values
+
+  for (int thisReading = 0; thisReading < numReadings; thisReading++) {
+    readings[thisReading] = 0;
+    delay(1);
+  }
+  
+}
+
+void updateAverage() {
+  // subtract the last reading:
+  total = total - readings[readIndex];
+  // read from the sensor:
+  readings[readIndex] = analogRead(ACC_Z_PIN);
+  // add the reading to the total:
+  total = total + readings[readIndex];
+  // advance to the next position in the array:
+  readIndex = readIndex + 1;
+
+  // if we're at the end of the array...
+  if (readIndex >= numReadings) {
+    // ...wrap around to the beginning:
+    readIndex = 0;
+  }
+
+  // calculate the average:
+  average = total / numReadings;
+  delay(1);        // delay in between reads for stability
 }
 
 void loop() {
+  updateAverage();
+  
   //If vibration detected, 3 dots
-  int z_reading = analogRead(ACC_Z_PIN);
-  if (z_reading > 550 || z_reading < 150) {
-    digitalWrite(TOP_DOT, HIGH);
-    digitalWrite(MIDDLE_DOT, HIGH);
-    digitalWrite(BOTTOM_DOT, HIGH);
-
-    clearDisplay();
-    //want to wait before checking again to make sure that design isn't over written when removing from vibration
-    delay(REFRESH_DELAY);
-
-  }
-  //no vibration detected, only 1 dot
-  else {
-    digitalWrite(TOP_DOT, LOW);
-    digitalWrite(MIDDLE_DOT, HIGH);
-    digitalWrite(BOTTOM_DOT, LOW);
-
-      //clear display to update the dots
-    clearDisplay();
-    delay(REFRESH_DELAY);
+  if ((average > 450) || (average < 300)) {
+    //only update state and delay if not already showing this display
+    if (current_state != 1){
+      current_state = 1;
+      digitalWrite(TOP_DOT, HIGH);
+      digitalWrite(MIDDLE_DOT, HIGH);
+      digitalWrite(BOTTOM_DOT, HIGH);
+  
+      clearDisplay();
+      //want to wait before checking again to make sure that design isn't over written when removing from vibration
+      delay(REFRESH_DELAY);
+      delay(REFRESH_DELAY);
+      delay(REFRESH_DELAY);
+      delay(REFRESH_DELAY);
+    }
+  } else{
+    if (current_state != 0){
+      current_state = 0;
+      digitalWrite(TOP_DOT, LOW);
+      digitalWrite(MIDDLE_DOT, HIGH);
+      digitalWrite(BOTTOM_DOT, LOW);
+  
+        //clear display to update the dots
+      clearDisplay();
+      delay(REFRESH_DELAY);
+    }
   }
 
 }
